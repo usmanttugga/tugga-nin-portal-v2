81f9a8e9b13b702435b1c283920707bedbfa0cc5
usmanttugga <usmantugga84@gmail.com>
2026-03-31 10:08:26 -0700

    feat: NIN Modification uses Modified button with PDF slip upload via Firebase Storage, user gets download link

--- js/firebase.js
+++ js/firebase.js
@@ -7,6 +7,9 @@ import {
   getFirestore, doc, setDoc, getDoc, onSnapshot,
   collection, addDoc, getDocs, query, where, updateDoc, increment
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  getStorage, ref, uploadBytes, getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDX31m2k1OOzedDQHAbpvgP15mxLjBRdwc",
@@ -20,9 +23,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 window.fbAuth = auth;
 window.fbDB = db;
+window.fbStorage = storage;
+window.fbStorageRef = ref;
+window.fbUploadBytes = uploadBytes;
+window.fbGetDownloadURL = getDownloadURL;
 window.fbDoc = doc;
 window.fbSetDoc = setDoc;
 window.fbGetDoc = getDoc;
